9165f7004e84a3f94e88710b904d08a186768321
JoshuaRosado <joshua06rosado@gmail.com>
2024-09-17 09:23:10 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Joshua Rosado Olivencia on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
