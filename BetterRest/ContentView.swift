30bb4feee1f679564b35c083de905846ae13a8b1
JoshuaRosado <joshua06rosado@gmail.com>
2024-09-17 10:36:34 -0500

    update

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -5,20 +5,33 @@
 //  Created by Joshua Rosado Olivencia on 9/17/24.
 //
 
+// ==========================================================
+
+
+// CORE ML = let's us use machine learning models in our app to make predictions
+
+// CREATE ML = let's us create custom machine learning models with our own data
+
+
+// Tabular Regression = Throwing a whole bunch of spread sheets data at CREATE ML and ask it to figure out the different relationship of various values for us
+
+
+// ML 2 STEPS
+
+// Step 1 : We train the model with CREATE ML
+// Step 2 : Ask model to make prediction with CORE ML
+
+// ==========================================================
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("BetterRest").font(.system(size: 30)).bold().foregroundStyle(.teal)
+        Text("loading...").foregroundStyle(.secondary)
+        
     }
 }
-
 #Preview {
     ContentView()
 }
